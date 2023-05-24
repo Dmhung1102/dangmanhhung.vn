92281e482abab195cf6af9502219b712f8aaf76a
dmcgithub315 <94636348+dmcgithub315@users.noreply.github.com>
2023-05-24 09:40:48 +0700

    laptop commit

--- delete.php
+++ delete.php
@@ -2,7 +2,10 @@
 include ('connect.php');
 $id = $_GET['id'];
 // lệnh sql xóa 1 bản ghi
-$sql = "DELETE FROM house WHERE id= $id";
+$sql = "DELETE house, housealbum FROM house 
+        INNER JOIN housealbum ON house.id = housealbum.houseid
+        WHERE house.id = '$id'";
+
 
 if ($conn->query($sql) === TRUE) {
     echo "Record deleted successfully <a href='index.php'>Go Home</a>";
